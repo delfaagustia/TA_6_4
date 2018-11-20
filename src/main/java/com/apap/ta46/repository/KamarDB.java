446a369af9a8ba98799cbe5bcdf03e3c15f57916
Hauri Silmi Zafirah <haurisilmizafirah@gmail.com>
2018-11-20 19:38:07 +0700

    create Kamar & Paviliun DB

---
+++ src/main/java/com/apap/ta46/repository/KamarDB.java
@@ -0,0 +1,11 @@
+package com.apap.ta46.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.lang.Long;
+import com.apap.ta46.model.KamarModel;
+
+@Repository
+public interface KamarDB extends JpaRepository<KamarModel, Long>{
+
+}
