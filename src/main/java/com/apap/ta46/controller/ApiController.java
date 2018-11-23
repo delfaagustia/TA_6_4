813300a6550b4e80c2b30c6ee4953d69a39cb241
riodejaneiro30 <riokurtinus@gmail.com>
2018-11-23 15:57:06 +0700

    doing fitur #9

--- src/main/java/com/apap/ta46/controller/ApiController.java
+++ src/main/java/com/apap/ta46/controller/ApiController.java
@@ -2,12 +2,8 @@ package com.apap.ta46.controller;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,7 +31,6 @@ public class ApiController {
         	response.setMessage("success");
         	response.setResult(kamarDb.findAll());
         }
-        System.out.println(response);
 
         return response;
     }
