4fb01b3965f4cbc6f2e8f4568b880b28288f0843
Muhammad Fawwaz Syarif <fawwaz.syarif19@gmail.com>
2018-12-08 13:56:38 +0700

    adding nama dokter #11

--- src/main/java/com/apap/ta46/controller/JadwalJagaController.java
+++ src/main/java/com/apap/ta46/controller/JadwalJagaController.java
@@ -1,13 +1,19 @@
 package com.apap.ta46.controller;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.apap.ta46.model.DokterModel;
 import com.apap.ta46.model.JadwalJagaModel;
+import com.apap.ta46.model.PasienModel;
+import com.apap.ta46.model.RequestPasienModel;
 import com.apap.ta46.service.DokterService;
 import com.apap.ta46.service.JadwalJagaService;
 
@@ -21,8 +27,19 @@ public class JadwalJagaController {
 	DokterService dokterService;
 	
 	@RequestMapping("")
-	public String viewAllJadwalJaga(Model model) {
+	public String viewAllJadwalJaga(Model model) throws IOException {
 		List<JadwalJagaModel> listAllJadwalJaga = jadwalJagaService.getAllJadwalJaga();
+		
+		Map<JadwalJagaModel, DokterModel> map = new HashMap<>();
+		
+		for(JadwalJagaModel jadwalJaga : listAllJadwalJaga) {
+			long idDokter = jadwalJaga.getIdDokter();
+			
+			map.put(jadwalJaga, dokterService.getDokterById(idDokter));
+		}
+		
+		model.addAttribute("map", map);
+		
 		model.addAttribute("listAllJadwalJaga", listAllJadwalJaga);
 		return "jadwal-jaga";
 	}
